cd51ab1610003d0e4a0a2e19ce3a781d815ffc2e
slava bily <slavabily@me.com>
2022-02-09 14:59:34 +0200

    defining protocol conforming types

--- SwiftProtocols.playground/Contents.swift
+++ SwiftProtocols.playground/Contents.swift
@@ -8,3 +8,36 @@ protocol Bird {
 protocol Flyable {
     var airspeedVelocity: Double { get }
 }
+
+struct FlappyBird: Bird, Flyable {
+    let name: String
+    let flappyAmplitude: Double
+    let flappyFrequency: Double
+    let canFly = true
+    
+    var airspeedVelocity: Double {
+        3 * flappyFrequency * flappyAmplitude
+    }
+}
+
+struct Penguin: Bird {
+    let name: String
+    let canFly = false
+}
+
+struct SwiftBird: Bird, Flyable {
+    var name: String { "Swift \(version)"}
+    var canFly = true
+    let version: Double
+    private var speedFactor = 1000.0
+    
+    init(version: Double) {
+        self.version = version
+    }
+    
+    // Swift is faster with each version!
+    
+    var airspeedVelocity: Double {
+        version * speedFactor
+    }
+}
